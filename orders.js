9ceabbb67430f2d13abce10136a25c12c4c7907d
Sailesh_Kathir_A <saileshayyappan001@gmail.com>
2022-02-18 14:46:48 +0000

    Dashboard

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders =[
+    {
+        productName:'Printed Urea Sacks',
+        productNumber:'85631',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Urea Sacks',
+        productNumber:'85632',
+        paymentStatus:'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName:'White Sacks',
+        productNumber:'85633',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Printed Urea Sacks',
+        productNumber:'85634',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName:'White Sacks',
+        productNumber:'85635',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    }
+]
