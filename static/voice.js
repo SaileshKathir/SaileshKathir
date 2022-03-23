0c0e728032f117c5752a6c087519d61479d504cf
lavan.s <sbgplg26@gmail.com>
2022-03-23 08:14:48 +0000

    text to speech

---
+++ static/voice.js
@@ -0,0 +1,12 @@
+// let speech = new SpeechSynthesisUtterance();
+// speech.lang = "en";
+// let voices = [];
+// speech.voice = voices[1];
+// speech.rate = 1;
+// speech.volume = 1;
+// speech.pitch = 10;
+
+// document.querySelector("#text-to-send").addEventListener("click", () => {
+//   speech.text = document.querySelector(item.message).value;
+//   window.speechSynthesis.speak(speech);
+// });
